fe5f8288c492db96ef07c9aa28b44b9b58b6102b
noahmuler <noahtaddese@gmail.com>
2022-12-22 14:25:27 -0800

    Hot Fix 3

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,32 +1,30 @@
 #!/usr/bin/python3
 """
-Distributes an archive to web servers
+Fabric script based on the file 1-pack_web_static.py that distributes an
+archive to the web servers
 """
 
-from fabric.api import run, put, env, sudo
-from os.path import isfile
-
-
+from fabric.api import put, run, env
+from os.path import exists
 env.hosts = ['100.26.170.160', '34.229.161.0']
 
 
 def do_deploy(archive_path):
-    """ Distributes an archive to web servers """
-    if not isfile(archive_path):
+    """distributes an archive to the web servers"""
+    if exists(archive_path) is False:
         return False
     try:
-        localpath = archive_path.split('/')[1]
-        newpath = localpath.split('.')[0]
-        rempath = "/data/web_static/releases/"
-
-        put(archive_path, "/tmp/".format(localpath))
-        sudo("mkdir -p {}{}".format(rempath, newpath))
-        sudo("tar -xzf /tmp/{} -C {}{}".format(localpath, rempath, newpath))
-        sudo("rm /tmp/{}".format(localpath))
-        sudo("cp -r {0}{1}/web_static/* {0}{1}/".format(rempath, newpath))
-        sudo("rm -rf {}{}/web_static".format(rempath, newpath))
-        sudo("rm -rf /data/web_static/current")
-        sudo("ln -s {}{}/ /data/web_static/current".format(rempath, newpath))
+        file_n = archive_path.split("/")[-1]
+        no_ext = file_n.split(".")[0]
+        path = "/data/web_static/releases/"
+        put(archive_path, '/tmp/')
+        run('mkdir -p {}{}/'.format(path, no_ext))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
+        run('rm /tmp/{}'.format(file_n))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
+        run('rm -rf {}{}/web_static'.format(path, no_ext))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
         return True
     except:
         return False
